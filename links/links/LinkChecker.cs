f8efd79879bd65aa0fee9788dd0bb97cf5948568
Firdavsi <firdavsinurov326@gmail.com>
2022-11-16 02:35:25 +0300

    lab 2

---
+++ links/links/LinkChecker.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text.RegularExpressions;
+using HtmlAgilityPack;
+
+
+namespace links
+{
+    internal class LinkChecker
+    {
+        private readonly string _domain;
+        private readonly HttpClient _client;
+        private readonly HashSet<string> _set;
+        
+        public LinkChecker(string d)
+        {
+            var uri = new Uri(d);
+            if (!IsUrl(uri.OriginalString))
+            {
+                Console.WriteLine(d, " not valid uri. Ex https://google.com");
+                return;
+            }
+            _domain = d;
+            var handler = new HttpClientHandler() { AutomaticDecompression = DecompressionMethods.All };
+            _client = new HttpClient(handler) { DefaultRequestVersion = HttpVersion.Version20 };
+            _client.DefaultRequestHeaders.UserAgent.ParseAdd("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.99 Safari/537.36");
+            _client.BaseAddress = uri;
+        }
+
+        public readonly Dictionary<string, int> ValidLinks = new();
+
+        public readonly Dictionary<string, int> InvalidLinks = new();
+
+        private static int GetStatusCode(string link)
+        {
+            try
+            {
+                var request = (HttpWebRequest)WebRequest.Create(link);
+                request.Method = WebRequestMethods.Http.Head;
+                request.AllowAutoRedirect = false;
+                request.Accept = @"*/*";
+                using var response = (HttpWebResponse)request.GetResponse();
+                return (int)response.StatusCode;
+            }
+            catch (WebException ex)
+            {
+                if (ex.Response == null)
+                    throw;
+                return (int)((HttpWebResponse)ex.Response).StatusCode;
+            }
+        }
+
+        private static IEnumerable<string> GetLinksFromPage(string link, Uri domain)
+        {
+            try
+            {
+                var client = new WebClient();
+                var html = client.DownloadString(link);
+                
+                var htmlSnippet = new HtmlDocument();
+                htmlSnippet.LoadHtml(html);
+
+                var links = new List<string>();
+
+                foreach (var a in htmlSnippet.DocumentNode.SelectNodes("//a[@href]"))
+                {
+                    var href = a.Attributes["href"];
+                    var validLink = GetUrlPath(href.Value, domain);
+                    if (validLink != "")
+                    {
+                        links.Add(validLink);   
+                    }
+                }
+
+                return links;
+                
+                // var reHref = new Regex(@"(?inx)
+                //  <a \s [^>]*
+                //     href \s* = \s*
+                //        (?<q> ['""] )
+                //            (?<url> [^""]+ )
+                //       \k<q>
+                //  [^>]* >");
+                //
+                // var hrefTags = new HashSet<string>();
+                // foreach (Match match in reHref.Matches(htmlCode)) hrefTags.Add(match.Groups["url"].ToString());
+                // var links = new HashSet<string>();
+                // foreach (var hrefTag in hrefTags)
+                // {
+                //     var validLink = GetUrlPath(hrefTag, domain);
+                //     if (validLink != "")
+                //     {
+                //         links.Add(GetUrlPath(hrefTag, domain));
+                //     }
+                // }
+                //
+                //
+                // return links; 
+            }
+            catch (Exception)
+            {
+                Console.WriteLine($"can't download html from {link}");
+                return new HashSet<string>();
+            }
+        }
+
+        private static bool IsValidLink(int status)
+        {
+            return ((status / 200 == 1) && (status % 200 < 100));
+            // return !((status / 400 == 1) && (status % 400 < 100));
+        }
+
+        public void CheckAllDomainLinks()
+        {
+            try
+            {
+                var uri = new Uri(_domain);
+                if (!IsUrl(uri.OriginalString))
+                {
+                    Console.WriteLine(_domain, " not valid uri. Ex https://google.com");
+                    return;
+                }
+
+                var unverifiedUrls = new HashSet<string> { _domain };
+                var verifiedUrls = new HashSet<string>();
+                while (unverifiedUrls.Count != 0)
+                {
+                    var s = unverifiedUrls.ElementAt(unverifiedUrls.Count - 1);
+                    unverifiedUrls.Remove(s);
+                    verifiedUrls.Add(s);
+                    Console.Write($"{s}\t");
+                    var status = GetStatusCode(s);
+                    Console.WriteLine(status);
+                    if (IsValidLink(status))
+                    {
+                        ValidLinks.Add(s, status);
+                        var links = GetLinksFromPage(s, uri);
+                        foreach (var link in links.Where(link => !verifiedUrls.Contains(link)))
+                        {
+                            unverifiedUrls.Add(link);
+                        }
+                    }
+                    else
+                    {
+                        InvalidLinks.Add(s, status);
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                Console.WriteLine(_domain, " not valid uri. Ex https://google.com");
+            }
+        }
+
+        private static bool IsUrl(string s)
+        {
+            const string pattern = @"^http(s)?://([\w-]+.)+[\w-]+(/[\w- ./?%&=])?(/)?$";
+            // var pattern =
+            // @"((http(s)?://)?([\w-]+\.)+[\w-]+[.com]+([\w\-\.,@?^=%&amp;:/~\+#]*[\w\-\@?^=%&amp;/~\+#])?)";
+            return Regex.IsMatch(s, pattern, RegexOptions.IgnoreCase);
+        }
+
+        private static string GetUrlPath(string s, Uri domain)
+        {
+            try
+            {
+                var u = new Uri(s);
+                var path = u.Scheme + "://" + u.Host + u.PathAndQuery;
+                return (IsUrl(path) && domain.Host == u.Host) ? path : "";
+            }
+            catch (Exception)
+            {
+                try
+                {
+                    var u = new Uri(domain, s);
+                    var path = u.Scheme + "://" + u.Host + u.PathAndQuery;
+                    return (IsUrl(path) && domain.Host == u.Host) ? path : "";
+                }
+                catch (Exception)
+                {
+                    return "";
+                }
+            }
+        }
+    }
+}
---
+++ links/links/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+using links;
+
+Console.Write("Enter link: ");
+var link = Console.ReadLine();
+
+// const string link = "http://links.qatl.ru/";
+// const string link = "https://www.yola-mkt.ru/";
+
+var lc = new LinkChecker(link);
+var startDate = DateTime.Now;
+lc.CheckAllDomainLinks();
+var finalDate = DateTime.Now; 
+
+
+const string validLinksPath = "valid2.txt";
+const string invalidLinksPath = "invalid2.txt";
+
+
+await using (var writer = new StreamWriter(validLinksPath, false))
+{
+    foreach (var l in lc.ValidLinks)
+    {
+        await writer.WriteLineAsync($"{l.Key}\t{l.Value}");
+    }
+    
+    await writer.WriteLineAsync($"Count: {lc.ValidLinks.Count} | Start: {startDate.Hour}:{startDate.Minute}:{startDate.Second} | Final: {finalDate.Hour}:{finalDate.Minute}:{finalDate.Second}");
+}
+
+await using (var writer = new StreamWriter(invalidLinksPath, false))
+{
+    foreach (var l in lc.InvalidLinks)
+    {
+        await writer.WriteLineAsync($"{l.Key}\t{l.Value}");
+    }
+    await writer.WriteLineAsync($"Count: {lc.InvalidLinks.Count} | Start: {startDate.Hour}:{startDate.Minute}:{startDate.Second} | Final: {finalDate.Hour}:{finalDate.Minute}:{finalDate.Second}");
+}
+
+Console.WriteLine("END");
+Console.ReadLine();
